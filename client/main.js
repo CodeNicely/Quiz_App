d0610602dad46875c2cda427c9358a93947f3431
onkar <onkarsahu670@gmail.com>
2017-11-20 20:34:40 +0530

    first commit

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import '../template/home.html';
+import '../template/gk.html';
+import '../template/maths.html';
+import '../template/english.html';
+import '../template/reasoning.html';
+import './main.html';
